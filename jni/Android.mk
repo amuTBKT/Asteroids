b68495d5e58b0962e293741005d4ed1de8c18de0
Amit kumar Mehar <gookutbkt@gmail.com>
2014-10-02 23:03:15 +0530

    adds native opengl framework

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := asteroids
-LOCAL_SRC_FILES := # name of classes to include
+LOCAL_MODULE := Asteroids
+LOCAL_SRC_FILES := Game.cpp 		# name of classes to include
+LOCAL_LDLIBS := -lGLESv2
 include $(BUILD_SHARED_LIBRARY)
--- src/com/amu/asteroids/CustomRenderer.java
+++ src/com/amu/asteroids/CustomRenderer.java
@@ -8,15 +8,23 @@ import android.opengl.GLSurfaceView.Renderer;
 public class CustomRenderer implements Renderer {
 
 	public void onSurfaceCreated(GL10 gl, EGLConfig egl) {
-
+		nativeSurfaceCreated();
 	}
 	
 	public void onDrawFrame(GL10 gl) {
-		
+		nativeDrawFrame();
 	}
 
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
-
+		nativeSurfaceChanged(width, height);
 	}
+	
+	private static native void nativeSurfaceCreated();
+	private static native void nativeDrawFrame();
+	private static native void nativeSurfaceChanged(int width, int height);
 
+	static {
+		System.loadLibrary("Asteroids");
+	}
+	
 }
